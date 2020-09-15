e416a4a04dd6b69f20a5f5cc5745889815c99020
allanrivers <allanrivers99@gmail.com>
2020-09-15 15:20:00 -0700

    Button for checking morning and night temps

--- weather-app/script.js
+++ weather-app/script.js
@@ -1,12 +1,9 @@
 // Input for search method
-const state = document.getElementById('state');
+const state = document.getElementById("state");
 
-state.addEventListener('change', (e) => {
-    const target = e.target.value;
-})
-
-
-
+state.addEventListener("change", (e) => {
+  const target = e.target.value;
+});
 
 // Fetch the data to send to showUI function
 const fetchData = () => {
@@ -14,7 +11,6 @@ const fetchData = () => {
     .then((res) => res.json())
     .then((data) => showUI(data));
 };
-
 
 // Create the UI using fetched data
 const showUI = (result) => {
@@ -26,7 +22,7 @@ const showUI = (result) => {
             <h4 class="nightTemp">Night: ${result.times[0].night}</h4>
         <div>
     </div>`;
-    document.querySelector('.cards').innerHTML += card;
+    document.querySelector(".cards").innerHTML += card;
   });
 
   return resultsReceived;
@@ -36,25 +32,42 @@ fetchData();
 
 // Change theme
 let tempVal = true;
-const btnTheme = document.querySelector('.changeTheme');
+const btnTheme = document.querySelector(".changeTheme");
 
+// ChangeTempTheme function
 const changeTempTheme = () => {
-    tempVal = !tempVal;
-    const morningTemp = document.querySelectorAll('.card')
-    if(tempVal) {
-        for(let i = 0; i < morningTemp.length; i++) {
-            morningTemp[i].setAttribute('style', 'background-color: #0093E9; background-image: linear-gradient(160deg, #0093E9 0%, #80D0C7 100%);')
-        }
-        btnTheme.innerText = 'Check night';
-    }   else {
-        for(let i = 0; i < morningTemp.length; i++) {
-            morningTemp[i].setAttribute('style', 'background-color: ##29049c; background-image: linear-gradient(180deg, #29049c 35%, #7618b1 60%, #b95aa3 89%); color: white;')
-        }
-        btnTheme.innerText = 'Check morning';
+  // change tempVal boolean to the opposite
+  tempVal = !tempVal;
+  // Variable for card class
+  const morningTemp = document.querySelectorAll(".card");
+
+  //
+  if (tempVal) {
+    for (let i = 0; i < morningTemp.length; i++) {
+      morningTemp[i].setAttribute(
+        "style",
+        "background-color: #0093E9; background-image: linear-gradient(160deg, #0093E9 0%, #80D0C7 100%);"
+      );
     }
-}
+    btnTheme.innerText = "Check night temp";
+    btnTheme.setAttribute(
+      "style",
+      "background-color: ##29049c; background-image: linear-gradient(180deg, #29049c 35%, #7618b1 60%, #b95aa3 89%); color: white;"
+    );
+  } else {
+    for (let i = 0; i < morningTemp.length; i++) {
+      morningTemp[i].setAttribute(
+        "style",
+        "background-color: ##29049c; background-image: linear-gradient(180deg, #29049c 35%, #7618b1 60%, #b95aa3 89%); color: white;"
+      );
+    }
+    btnTheme.innerText = "Check morning temp";
+    btnTheme.setAttribute(
+      "style",
+      "background-color: #0093E9; background-image: linear-gradient(160deg, #0093E9 0%, #80D0C7 100%); color: #333;"
+    );
+  }
+};
 
-btnTheme.addEventListener('click', changeTempTheme)
-
-
-
+// Call event on click
+btnTheme.addEventListener("click", changeTempTheme);
